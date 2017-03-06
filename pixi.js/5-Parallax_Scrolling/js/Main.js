84e41628564f4d13abbfe6ba718c4253bf66f3b0
William Woodard <wwoodar1@uncc.edu>
2017-03-06 14:22:02 -0500

    Pixi.js Parallax Scrolling 3 Wall Sprites Pool - Remove Step Test

--- pixi.js/5-Parallax_Scrolling/js/Main.js
+++ pixi.js/5-Parallax_Scrolling/js/Main.js
@@ -31,9 +31,6 @@ Main.prototype.spriteSheetLoaded = function () {
   this.scroller = new Scroller(this.stage);
 
   requestAnimationFrame(this.update.bind(this));
-
-  this.pool = new WallSpritesPool();
-  this.wallSlices = [];
 };
 
 Main.prototype.borrowWallSprites = function (num) {
@@ -64,59 +61,6 @@ Main.prototype.returnWallSprites = function () {
     } else {
       this.pool.returnDecoration(sprite);
     }
-  }
-
-  this.wallSlices = [];
-};
-
-Main.prototype.generateTestWallSpan = function () {
-  var lookupTable = [
-    this.pool.borrowFrontEdge,  // 1st slice
-    this.pool.borrowWindow,     // 2nd slice
-    this.pool.borrowDecoration, // 3rd slice
-    this.pool.borrowStep,       // 4th slice
-    this.pool.borrowWindow,     // 5th slice
-    this.pool.borrowBackEdge    // 6th slice
-  ];
-
-  var yPos = [
-    128, // 1st slice
-    128, // 2nd slice
-    128, // 3rd slice
-    192, // 4th slice
-    192, // 5th slice
-    192  // 6th slice
-  ];
-
-  for (var i = 0; i < lookupTable.length; i++) {
-    var func = lookupTable[i];
-
-    var sprite = func.call(this.pool);
-    sprite.position.set(64 + (i * 64), yPos[i]);
-
-    this.wallSlices.push(sprite);
-
-    this.stage.addChild(sprite);
-  }
-};
-
-Main.prototype.clearTestWallSpan = function () {
-  var lookupTable = [
-    this.pool.returnFrontEdge,  // 1st slice
-    this.pool.returnWindow,     // 2nd slice
-    this.pool.returnDecoration, // 3rd slice
-    this.pool.returnStep,       // 4th slice
-    this.pool.returnWindow,     // 5th slice
-    this.pool.returnBackEdge    // 6th slice
-  ];
-
-  for (var i = 0; i < lookupTable.length; i++) {
-    var func = lookupTable[i];
-    var sprite = this.wallSlices[i];
-
-    this.stage.removeChild(sprite);
-
-    func.call(this.pool, sprite);
   }
 
   this.wallSlices = [];
