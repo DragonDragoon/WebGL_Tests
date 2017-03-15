410126a281bcfdcca54525f2a2f61011e467952b
William Woodard <wwoodar1@uncc.edu>
2017-03-15 10:46:37 -0400

    Scroll Acceleration

--- pixi.js/5-Parallax_Scrolling/js/Main.js
+++ pixi.js/5-Parallax_Scrolling/js/Main.js
@@ -7,13 +7,23 @@ function Main() {
     }
   );
 
+  this.scrollSpeed = Main.MIN_SCROLL_SPEED;
+
   this.loadSpriteSheet();
 }
 
-Main.SCROLL_SPEED = 5;
+Main.MIN_SCROLL_SPEED = 5;
+Main.MAX_SCROLL_SPEED = 15;
+Main.SCROLL_ACCELERATION = 0.005;
 
 Main.prototype.update = function () {
-  this.scroller.moveViewportXBy(Main.SCROLL_SPEED);
+  this.scroller.moveViewportXBy(this.scrollSpeed);
+
+  this.scrollSpeed += Main.SCROLL_ACCELERATION;
+  if (this.scrollSpeed > Main.MAX_SCROLL_SPEED) {
+    this.scrollSpeed = Main.MAX_SCROLL_SPEED;
+  }
+
   this.renderer.render(this.stage);
   requestAnimationFrame(this.update.bind(this));
 };
