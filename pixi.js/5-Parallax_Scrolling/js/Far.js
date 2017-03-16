1cd43aa5a2b3c73cb623d7f0e5eaa314ec03a9f6
William Woodard <wwoodar1@uncc.edu>
2017-03-16 13:05:27 -0400

    Far.js: ES6ify

--- pixi.js/5-Parallax_Scrolling/js/Far.js
+++ pixi.js/5-Parallax_Scrolling/js/Far.js
@@ -1,19 +1,20 @@
-function Far() {
-  var texture = PIXI.Texture.fromImage('res/bg-far.png');
-  PIXI.extras.TilingSprite.call(this, texture, 512, 256);
+class Far extends PIXI.extras.TilingSprite {
+  constructor() {
+    let texture = PIXI.Texture.fromImage('res/bg-far.png');
+    
+    super(texture, 512, 256);
 
-  this.position.set(0, 0);
-  this.tilePosition.set(0, 0);
+    this.position.set(0, 0);
+    this.tilePosition.set(0, 0);
 
-  this.viewportX = 0;
+    this.viewportX = 0;
+  }
+
+  setViewportX(newViewportX) {
+    let distanceTravelled = newViewportX - this.viewportX;
+    this.viewportX = newViewportX;
+    this.tilePosition.x -= (distanceTravelled * Far.DELTA_X);
+  }
 }
 
-Far.prototype = Object.create(PIXI.extras.TilingSprite.prototype);
-
 Far.DELTA_X = 0.064;
-
-Far.prototype.setViewportX = function(newViewportX) {
-  var distanceTravelled = newViewportX - this.viewportX;
-  this.viewportX = newViewportX;
-  this.tilePosition.x -= (distanceTravelled * Far.DELTA_X);
-};
