ef79d50f85b98808b8a7a5c74c69088d6edc9955
William Woodard <wwoodar1@uncc.edu>
2017-03-16 13:17:03 -0400

    WallSlice.js: ES6ify

--- pixi.js/5-Parallax_Scrolling/js/WallSlice.js
+++ pixi.js/5-Parallax_Scrolling/js/WallSlice.js
@@ -1,7 +1,9 @@
-function WallSlice(type, y) {
-  this.type = type;
-  this.y = y;
-  this.sprite = null;
+class WallSlice {
+  constructor(type, y) {
+    this.type = type;
+    this.y = y;
+    this.sprite = null;
+  }
 }
 
 WallSlice.WIDTH = 64;
