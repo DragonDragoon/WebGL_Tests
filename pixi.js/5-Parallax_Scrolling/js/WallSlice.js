7256efb88500a87e1db439e044c35f2a61a63871
William Woodard <wwoodar1@uncc.edu>
2017-03-16 15:18:39 -0400

    WallSlice.js: Documentation

--- pixi.js/5-Parallax_Scrolling/js/WallSlice.js
+++ pixi.js/5-Parallax_Scrolling/js/WallSlice.js
@@ -1,4 +1,20 @@
+/**
+ * @name WallSlice.js
+ * @author William Woodard
+ * @desc Data structure class to hold wall slice information
+ *        @class {WallSlice}
+ *          @constant WIDTH
+ * @required none
+ */
 class WallSlice {
+  // Hopefully the ES7 specfication will give us better class implementation
+  // Still have to put const/static outside of class, after it it declared (see bottom)
+
+  /**
+   * @constructor WallSlice.constructor
+   * @desc Create wall slice data structure for type, y-coordinate, and sprite
+   * @return null
+   */
   constructor(type, y) {
     this.type = type;
     this.y = y;
@@ -6,4 +22,5 @@ class WallSlice {
   }
 }
 
+// Constants (Why do I still have to do this in ES6? Don't call these classes!)
 WallSlice.WIDTH = 64;
