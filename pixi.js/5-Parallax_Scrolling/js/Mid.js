152c65cba497ed8034b71cbd72a36d7d94a3ecf1
William Woodard <wwoodar1@uncc.edu>
2017-03-16 13:03:07 -0400

    Mid.js: ES6ify

--- pixi.js/5-Parallax_Scrolling/js/Mid.js
+++ pixi.js/5-Parallax_Scrolling/js/Mid.js
@@ -1,19 +1,20 @@
-function Mid() {
-  var texture = PIXI.Texture.fromImage('res/bg-mid.png');
-  PIXI.extras.TilingSprite.call(this, texture, 512, 256);
+class Mid extends PIXI.extras.TilingSprite {
+  constructor() {
+    let texture = PIXI.Texture.fromImage('res/bg-mid.png');
+    
+    super(texture, 512, 256);
 
-  this.position.set(0, 128);
-  this.tilePosition.set(0, 0);
+    this.position.set(0, 128);
+    this.tilePosition.set(0, 0);
 
-  this.viewportX = 0;
+    this.viewportX = 0;
+  }
+
+  setViewportX(newViewportX) {
+    let distanceTravelled = newViewportX - this.viewportX;
+    this.viewportX = newViewportX;
+    this.tilePosition.x -= (distanceTravelled * Mid.DELTA_X);
+  }
 }
 
-Mid.prototype = Object.create(PIXI.extras.TilingSprite.prototype);
-
 Mid.DELTA_X = 0.32;
-
-Mid.prototype.setViewportX = function(newViewportX) {
-  var distanceTravelled = newViewportX - this.viewportX;
-  this.viewportX = newViewportX;
-  this.tilePosition.x -= (distanceTravelled * Mid.DELTA_X);
-};
